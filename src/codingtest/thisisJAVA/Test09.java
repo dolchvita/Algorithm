37ff2fbf95b2cb3bce8bc640436c55dbe1fc9c40
류예서 <dolchvita@lyuyeseos-MacBook-Air.local>
2023-04-12 12:05:50 +0900

    0412

--- src/codingtest/thisisJAVA/Test09.java
+++ src/codingtest/thisisJAVA/Test09.java
@@ -7,17 +7,18 @@ public class Test09 {
 	public int[] solution(int n) {
 		int[] answer=new int[n];
 		
-		for(int i=0; i<n; i++) {
-			answer[i]=i;
-			
+		for(int i=1; i<=n; i++) {
+			answer[i-1]=i;
 		}
-		
+		for(int i=0; i<answer.length; i++) {
+			System.out.println(answer[i]);
+		}
 		return answer;
 	}
 	
 	public static void main(String[] args) {
 		Test09 test9=new Test09();
-		System.out.println(test9.solution(3));
+		System.out.println(test9.solution(5));
 	}
 
 }
